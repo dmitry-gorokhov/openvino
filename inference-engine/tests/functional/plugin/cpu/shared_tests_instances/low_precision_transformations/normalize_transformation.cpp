84ad543f0f3fb6e885040418c3f85fd8c3369eb9
Edward Shogulin <edward.shogulin@intel.com>
2020-05-27 11:17:56 +0300

    [LPT] NormalizeL2 transformation tests improvement

--- inference-engine/tests/functional/plugin/cpu/shared_tests_instances/low_precision_transformations/normalize_transformation.cpp
+++ inference-engine/tests/functional/plugin/cpu/shared_tests_instances/low_precision_transformations/normalize_transformation.cpp
@@ -8,6 +8,7 @@
 #include "common_test_utils/test_constants.hpp"
 
 using namespace LayerTestsDefinitions;
+using namespace InferenceEngine::details;
 
 namespace {
 const std::vector<InferenceEngine::Precision> netPrecisions = {
@@ -15,10 +16,20 @@ const std::vector<InferenceEngine::Precision> netPrecisions = {
         InferenceEngine::Precision::FP16
 };
 
+const std::vector<LayerTransformation::Params> trasformationParamValues = {
+    LayerTestsUtils::LayerTransformationParamsFactory::createParamCpu(),
+    LayerTestsUtils::LayerTransformationParamsFactory::createParamI8I8(),
+    LayerTestsUtils::LayerTransformationParamsFactory::createParamU8I8()
+};
+
+const std::vector<bool> fuseMultiply = { true, false };
+
 INSTANTIATE_TEST_CASE_P(LPT, NormalizeTransformation,
     ::testing::Combine(
         ::testing::ValuesIn(netPrecisions),
         ::testing::Values(InferenceEngine::SizeVector({ 1, 96, 32, 32 })),
-        ::testing::Values(CommonTestUtils::DEVICE_CPU)),
+        ::testing::Values(CommonTestUtils::DEVICE_CPU),
+        ::testing::ValuesIn(trasformationParamValues),
+        ::testing::ValuesIn(fuseMultiply)),
     NormalizeTransformation::getTestCaseName);
 }  // namespace
--- inference-engine/tests/functional/plugin/gpu/shared_tests_instances/low_precision_transformations/normalize_transformation.cpp
+++ inference-engine/tests/functional/plugin/gpu/shared_tests_instances/low_precision_transformations/normalize_transformation.cpp
@@ -8,6 +8,7 @@
 #include "common_test_utils/test_constants.hpp"
 
 using namespace LayerTestsDefinitions;
+using namespace InferenceEngine::details;
 
 namespace {
 const std::vector<InferenceEngine::Precision> netPrecisions = {
@@ -15,11 +16,19 @@ const std::vector<InferenceEngine::Precision> netPrecisions = {
         InferenceEngine::Precision::FP16
 };
 
+const std::vector<LayerTransformation::Params> trasformationParamValues = {
+    LayerTestsUtils::LayerTransformationParamsFactory::createParamGpu()
+};
+
+const std::vector<bool> fuseMultiply = { true, false };
+
 // The test is disabled because CLDNN does not have a Normalize layer implementation in INT8
 INSTANTIATE_TEST_CASE_P(DISABLED_LPT, NormalizeTransformation,
     ::testing::Combine(
         ::testing::ValuesIn(netPrecisions),
         ::testing::Values(InferenceEngine::SizeVector({ 1, 512, 32, 32 })),
-        ::testing::Values(CommonTestUtils::DEVICE_GPU)),
+        ::testing::Values(CommonTestUtils::DEVICE_GPU),
+        ::testing::ValuesIn(trasformationParamValues),
+        ::testing::ValuesIn(fuseMultiply)),
     NormalizeTransformation::getTestCaseName);
 }  // namespace
